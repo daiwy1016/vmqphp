51a6817c26d36514f3dfe06696dbf943b3c80f27
daiwy1016 <408366645@qq.com>
2021-01-19 18:11:39 +0800

    clear

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -633,15 +633,9 @@ class Index
         }        
         echo "success";           
         //继续业务流程
-        //回调通知发货
-        //http://192.168.1.103:7878/paycallback/ykpaybankwx.html?opstate=0&orderid=10104787105850005&ovalue=25&sign=8c99a8fcdc0527823d09612983bf02ae&msg=2123
+        //回调通知发货        
         $re = $this->getCurl("http://zf.963my.com/callback.php?".http_build_query($_GET));
-        exit;
-        $orderParams = json_decode(base64_decode($param),true);        
-        $content = "orderid=".$orderParams['orderid']."&opstate=0&ovalue=".$orderParams['value']."17cdbbfc9ba653f34a5ef9e8f86d7634";
-        $sign = md5($content);
-        $url = $orderParams['callbackurl']."?opstate=0&orderid=".$orderParams['orderid']."&ovalue=".$orderParams['value']."&sign=".$sign;   
-        $re = $this->getCurl($url);
+        exit;  
         //if($re == 'opstate=0'){  echo "success";       }
         //echo "商户订单号：".$payId ."<br>自定义参数：". $param ."<br>支付方式：". $type ."<br>订单金额：". $price ."<br>实际支付金额：". $reallyPrice;
         
